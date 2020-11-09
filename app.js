5fd895cf207a13bd6f4b9f4e65db994864c77b2a
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2020-11-09 10:40:15 +0300

    changed route

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/tasks', tasksRouter);
+app.use('/api/v1/', indexRouter);
+app.use('/api/v1/tasks', tasksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
